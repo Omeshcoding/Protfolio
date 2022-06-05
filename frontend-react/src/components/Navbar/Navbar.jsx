57c02dc58e2fe90b52d76beb303ba306ad53e190
umesh <umesharma.us11@gmail.com>
2022-06-05 23:00:18 +0530

    building menubar

--- frontend-react/src/components/Navbar/Navbar.jsx
+++ frontend-react/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React ,{ useState } from 'react';
+import { HiMenuAlt4, HiX } from 'react-icons/hi';
+import { motion } from 'framer-motion';
 import { images } from '../../constants';
 import './Navbar.scss';
 
 
 const Navbar = () => {
+    const [toggle, setToggle] = useState(false)
+
   return (
     <nav className='app__navbar'>
       <div className='app__navbar-logo'>
@@ -18,6 +22,30 @@ const Navbar = () => {
 
         ))}
       </ul>
+      <div className='app__navbar-menu'>
+        {/*Adding a callback function for toggle menu 
+      using the state , true so we can open it */}
+        <HiMenuAlt4 onClick={() => setToggle(true)}/>
+       {/* if toggle is true than we render, 
+      dynamic code block*/}
+         
+        {toggle && (
+            <motion.div
+            whileInView={{ x: [300, 0] }}
+            transition ={ {duration: 0.85, ease: 'easeOut'}}
+          >
+            <HiX onClick={() => setToggle(false)} />
+             {['home','about','work','skills','contact'].map((item) => (
+          <li key={item}>
+            <div />
+            <a href={`#${item}`} onClick={() => setToggle(false)}>{item}</a>
+          </li>
+
+        ))}
+            </motion.div>
+          )
+        }
+      </div>
     </nav>
   )
 }
