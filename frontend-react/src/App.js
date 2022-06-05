9ae5694678b1133f5b3fd6f8d677c788ca7f32ae
umesh <umesharma.us11@gmail.com>
2022-06-05 20:11:12 +0530

    assets added

--- frontend-react/src/App.js
+++ frontend-react/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
-
+import './App.scss';
 import { About, Footer, Header, Skills, Testimonial, Work } from './container';
-
 import { Navbar } from './components'
+
 
 const App = () => {
   return (
