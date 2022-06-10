d5d428e2db2e80cf0898c39f7e7c9fb60f3b2371
umesh <umesharma.us11@gmail.com>
2022-06-10 22:03:13 +0530

    styling hero

--- frontend-react/src/container/Header/Header.jsx
+++ frontend-react/src/container/Header/Header.jsx
@@ -18,7 +18,7 @@ const scaleVariants = {
 
 const Header = () => {
   return (
-    <div className='app__header app__flex'>
+    <div id = "home"  className='app__header app__flex'>
       <motion.div 
         whileInView={{ x: [-100, 0], opacity: [0, 1] }}
         transition={{ duration: 0.5 }}
@@ -59,12 +59,13 @@ const Header = () => {
         whileInView={scaleVariants.whileInView}
         className ="app__header-circles"
       >
-        {[images.flutter, images.redux, images.sass].map((circle, index) =>
-          <div className="circle-cmp app-flex" key={`circle - ${index}`}>
-         
-         <img src={circle} alt ="circle"/>
+        {[images.flutter, images.redux, images.sass].map((circle, index) => (
+          
+          <div className="circle-cmp app__flex" key={`circle - ${index}`}>
+          
+          <img src={circle} alt ="circle"/>
           </div>
-          )}
+          ))}
       </motion.div>
         
         
