d6891418578480597a8321a061eaef5221fa13bb
umesh <umesharma.us11@gmail.com>
2022-06-12 22:35:40 +0530

    building about section

--- frontend-react/src/container/About/About.jsx
+++ frontend-react/src/container/About/About.jsx
@@ -1,11 +1,30 @@
 import React from 'react'
+import { motion} from 'framer-motion'
 import './About.scss';
 
 
+cosnt abouts = [
+  { title: 'Web Development', description: 'I am a good web developer', imgUrl: '' }
+  { title: 'Web Development', description: 'I am a good web developer', imgUrl: '' }
+  { title: 'Web Development', description: 'I am a good web developer', imgUrl: '' }
+  { title: 'Web Development', description: 'I am a good web developer', imgUrl: '' }
+]
+
 const About = () => {
   return (
-    <div>about</div>
-  )
+    <>
+      <h2 className='head-text'>
+        I Know that
+        <span>Good Design</span>
+        <br />
+        means
+        <span>Good Business</span>
+      </h2>
+      <div className='app__profiles'>
+      
+      </div>
+    </>
+    )
 }
 
 export default About
