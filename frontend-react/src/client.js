877016549d4ab7acc4922068d77b52f2f0cae97d
umesh <umesharma.us11@gmail.com>
2022-06-17 21:56:45 +0530

    connecting with backend

--- frontend-react/src/client.js
+++ frontend-react/src/client.js
@@ -1,8 +1,8 @@
 import sanityClient from '@sanity/client';
-import imageUrlBuilder from '@sanity/image-url'
+import imageUrlBuilder from '@sanity/image-url';
 
 export const client = sanityClient({
-    projectId: process.env.REACT_APP_SANITY_PROJET_ID,
+    projectId: process.env.REACT_APP__SANITY_PROJECT_ID,
     dataset: 'production',
     apiVersion: '2022-02-01',
     useCdn: true,
@@ -10,3 +10,4 @@ export const client = sanityClient({
 });
 
 const builder = imageUrlBuilder(client);
+export const urlFor = (source) => builder.image(source);
