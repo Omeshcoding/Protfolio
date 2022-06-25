39340141062cff293c02934daa8981c97dc2b7bc
umesh <umesharma.us11@gmail.com>
2022-06-25 11:46:42 +0530

    bug fixes

--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -7,9 +7,49 @@ import { urlFor, client} from '../../client';
 
 
 const Skills = () => {
+
+  // State hooks
+  const [experiences, setExperiences] = useState([]);
+  const [skills, setSkills] = useState([]);
+
+
+  // Effects hook
+  useEffect(() => {
+    const query = '*[_type == "experiences"]';
+    const skillsQuery = '*[_type == "skills"]';
+
+    client.fetch(query)
+      .then((data) => {
+        setExperiences(data);
+    })
+
+    client.fetch(skillsQuery)
+      .then((data) => {
+        setSkills(data);
+    })
+  }, [])
+  
+
   return (
     <>
-    <h2 className='head-text'>Skills & Experience</h2>
+      <h2 className='head-text'>Skills & Experience</h2>
+      
+      <div className='app__skills-container'>
+        <motion.div className='app__skills-list'>
+          {skills.map((skill) => (
+            <motion.div
+              whileInView={{ opacity: [0, 1] }}
+              transition={{ duration: 0.5 }}
+              className="app__skills-item app__flex"
+              key={skill.name}
+            >
+              <div className='app__flex' style={{ background: skill.bgcolor }}>
+                <img src={urlFor(skill.icon)}/>              
+              </div>
+            </motion.div>
+        ))}
+        </motion.div>
+      </div>
     </>
   )
 }
