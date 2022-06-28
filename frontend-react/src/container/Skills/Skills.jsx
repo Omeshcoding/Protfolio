7cbee5e3bb450a30be08f142930a4632aa7a16f7
umesh <umesharma.us11@gmail.com>
2022-06-28 12:46:44 +0530

    styling exp section

--- frontend-react/src/container/Skills/Skills.jsx
+++ frontend-react/src/container/Skills/Skills.jsx
@@ -42,16 +42,53 @@ const Skills = () => {
               transition={{ duration: 0.5 }}
               className="app__skills-item app__flex"
               key={skill.name}
-            >
+              >
               <div className='app__flex' style={{ background: skill.bgcolor }}>
-                <img src={urlFor(skill.icon)}/>              
+              <img src={urlFor(skill.icon)}/>              
               </div>
-            </motion.div>
-        ))}
+              <p className='p-text'>{skill.name }</p>
+              </motion.div>
+              ))}
+              </motion.div>
+        <motion.div className='app__skills-exp'>
+              {experiences?.map((experience) => (
+                <motion.div
+                  className='app__skills-exp-item'
+                  key={experience.year}
+                >
+                  <div className='app__skills-exp-year'>
+                    <p className='bold-text'>{ experience.year }</p>
+                  </div>
+                  <motion.div className='app__skills-exp-works'>
+                    {experience.works.map((work) => (
+                    
+                               <>
+                                       <motion.div
+                                       whileInView={{ opacity: [0, 1] }}
+                                       transition={{ duration: 0.5 }}
+                                       className="app__skills-exp-work"
+                                       data-tip   
+                                       data-for = {work.name}    
+                                       key={work.name}
+                                         >
+                                           <h4 className='bold-text'>{ work.name }</h4>
+                                           <p className='p-text'>{ work.company }</p>
+                                         </motion.div>
+                                       {/*   <ReacTooltip
+                                      //      id={work.name}
+                                      //      effect="solid"
+                                      //      arrowColor='#fff'
+                                      //      className= 'skills-tooltip'
+                                      //       </ReacTooltip>*/}
+                      </>
+                  ))}
+                  </motion.div>
+                </motion.div>
+                ))}
         </motion.div>
       </div>
     </>
   )
 }
 
-export default Skills
+export default AppWrap(Skills ,'skills')
